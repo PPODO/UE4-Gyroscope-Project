f38f01ba86b159be9f685daba9ada3278d55fce6
PPODO <sherlockwhooo@gmail.com>
2021-02-08 21:54:22 +0900

    Update PlayerController

--- Source/GyroTest/Private/PlayerController/GyroPlayerController.cpp
+++ Source/GyroTest/Private/PlayerController/GyroPlayerController.cpp
@@ -1,11 +1,18 @@
 #include "PlayerController/GyroPlayerController.h"
 #include <GyroTest/Public/Components/SocketComponent.h>
 #include <Engine/Engine.h>
+#include "..\..\Public\PlayerController\GyroPlayerController.h"
 
 AGyroPlayerController::AGyroPlayerController() {
 	m_pSocketComponent = CreateDefaultSubobject<USocketComponent>(TEXT("Socket Component"));
 
+	m_currentInputMotionData = EInputMotion::EInput_Tilt;
+
 	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AGyroPlayerController::ChangeInputMotionData(EInputMotion inputMotion) {
+	m_currentInputMotionData = inputMotion;
 }
 
 void AGyroPlayerController::BeginPlay() {
@@ -18,6 +25,29 @@ void AGyroPlayerController::Tick(float fDeltaTime) {
 	Super::Tick(fDeltaTime);
 
 	if (m_pSocketComponent) {
-		m_pSocketComponent->SendTo(reinterpret_cast<uint8*>(TCHAR_TO_UTF8(TEXT("1"))), 2, *m_pSendAddr);
+		FVector vTilt, vRotationRate, vGravity, vAcceleration;
+		GetInputMotionState(vTilt,vRotationRate, vGravity, vAcceleration);
+
+		FVector vData;
+		switch (m_currentInputMotionData) {
+		case EInputMotion::EInput_Tilt:
+			vData = FMath::RadiansToDegrees(vTilt);
+			break;
+		case EInputMotion::EInput_RotationRate:
+			vData = FMath::RadiansToDegrees(vRotationRate);
+			break;
+		case EInputMotion::EInput_Gravity:
+			vData = vGravity;
+			break;
+		case EInputMotion::EInput_Acceleration:
+			vData = FMath::RadiansToDegrees(vAcceleration);
+			break;
+		}
+
+		FInputMotionData motionData(m_currentInputMotionData, vData);
+		GEngine->AddOnScreenDebugMessage(8, 1.f, FColor::Yellow, vData.ToString());
+
+		if (!m_pSocketComponent->SendTo(reinterpret_cast<uint8*>(&motionData), sizeof(FInputMotionData), *m_pSendAddr))
+			GEngine->AddOnScreenDebugMessage(10, 10.f, FColor::Red, FString::Printf(TEXT("Error! - Send failure, error code is %d"), m_pSocketComponent->GetLastErrorCode()));
 	}
 }
--- Source/GyroTest/Public/PlayerController/GyroPlayerController.h
+++ Source/GyroTest/Public/PlayerController/GyroPlayerController.h
@@ -4,11 +4,33 @@
 #include "GameFramework/PlayerController.h"
 #include "GyroPlayerController.generated.h"
 
+UENUM(BlueprintType)
+enum class EInputMotion : uint8 {
+	EInput_Tilt,
+	EInput_RotationRate,
+	EInput_Gravity,
+	EInput_Acceleration
+};
+
+struct FInputMotionData {
+public:
+	EInputMotion m_MotionType;
+	FVector m_vData;
+
+public:
+	FInputMotionData() : m_MotionType(EInputMotion::EInput_Tilt), m_vData(FVector::ZeroVector) {};
+	FInputMotionData(EInputMotion motionType, const FVector& vData) : m_MotionType(motionType), m_vData(vData) {};
+
+};
+
 UCLASS()
 class GYROTEST_API AGyroPlayerController : public APlayerController {
 	GENERATED_BODY()
 public:
 	AGyroPlayerController();
+
+	UFUNCTION(BlueprintCallable)
+		void ChangeInputMotionData(EInputMotion inputMotion);
 
 protected:
 	virtual void BeginPlay() override;
@@ -19,5 +41,7 @@ private:
 		class USocketComponent* m_pSocketComponent;
 
 	TSharedPtr<FInternetAddr> m_pSendAddr;
+
+	EInputMotion m_currentInputMotionData;
 
 };
